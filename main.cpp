6f175594b5b51c999f15ccfda0176ff65527f7df
b-michen1996 <benjamin.michen.96@gmail.com>
2023-10-18 16:11:17 +0200

    Commit for initialization

---
+++ main.cpp
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/main.cc to edit this template
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: bm
+ *
+ * Created on October 18, 2023, 3:59 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    int a = 10;
+
+    return 0;
+}
+
